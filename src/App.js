79e12208293a667cf0df8d4de4f9b30465f59f61
Edita <edita.tonoyan96@gmail.com>
2021-03-23 12:50:24 +0400

    commit to lesson-18 branch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,57 @@
 import './App.css';
+import{Route,Redirect,Switch} from 'react-router-dom';
 import React from 'react'
-import Todo from  './components/todo/Todo';
-
-
-
-
+import Todo from  './components/Pages/todo/Todo';
+import Contact from './components/Pages/Contact';
+import AboutUs from './components/Pages/AboutUs';
+import Navbar from './components/Navbar/Navbar';
+const router = [
+  { 
+    path: "/",
+    component : Todo,
+    exact: true
+  },
+  {
+    path: "/contact",
+    component: Contact,
+    exact: true
+  },
+  {
+    path: "/about_us",
+    component:AboutUs,
+    exact: true
+  }
+]
 export default function App() {
+
+ const pages = router.map((page,index)=>{
   return (
-    <div className="container">
-            <Todo />
-    
-    </div>
+            <Route
+              key = {index}
+              path={page.path}
+              component = {page.component}
+              exact={page.exact}
+            />
+ 
+
   )
+})
+  return (
+  
+    <div className="container">
+            <Navbar/>
+            <Switch>
+               {pages}
+               <Redirect to="/"/>
+            </Switch>
+
+    </div>
+
+  )
+
+  
 }
+ 
+
+
 
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import {Nav} from 'react-bootstrap';
+import {NavLink} from 'react-router-dom';
+
+const navlinks = [
+    {
+        to:"/",
+        exact:true,
+        value:"Home"
+    },
+    {
+        to:"/contact",
+        exact:true,
+        value:"Contact"
+    },
+    {
+        to:"/about_us",
+        exact:true,
+        value:"About"
+    }
+
+]
+export default function Navbar() {
+    const getnavLink = navlinks.map((navlink,index) => {
+        return (
+            <Nav.Item key = {index}>
+            <NavLink 
+                to={navlink.to}
+                className="nav-link"
+                activeStyle={{color:'red'}}
+                exact={navlink.exact}
+             >
+                 {navlink.value}
+            </NavLink>
+        </Nav.Item>
+        )
+    })
+    return (
+        <div>
+           
+            <Nav>
+                {getnavLink}
+            </Nav>
+        </div>
+    )
+}
